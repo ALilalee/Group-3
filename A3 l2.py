63e7d89210d1b1059e2a0d5a29cb7bfd251ff5cf
ALilalee <ali.lalee@yahoo.com>
2022-03-05 18:18:30 -0500

    data

--- A3 l2.py
+++ A3 l2.py
@@ -1,4 +1,5 @@
-mes = "Hello world"
-print(mes)
-hi
+hairstyles = ["bouffant", "pixie", "dreadlocks", "crew", "bowl", "bob", "mohawk", "flattop"]
 
+prices = [30, 25, 40, 20, 20, 35, 50, 35]
+
+last_week = [2, 3, 5, 8, 4, 4, 6, 2]
