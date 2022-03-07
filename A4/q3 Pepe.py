3904c324a87260fd11b9d8ccd8072a4316bb9a4c
ALilalee <ali.lalee@yahoo.com>
2022-03-06 20:39:33 -0500

    direction

--- A4/q3 Pepe.py
+++ A4/q3 Pepe.py
@@ -1,1 +1,18 @@
 hello
+# 3. Larger List
+# Let’s say we are working with two conveyor belts that contain items represented by a numerical ID. If one conveyor belt contains more items than the other, then we need to return the ID of the last item on that belt. In the case where they have the same number of items, return the last item from the first conveyor belt. In our code, we can represent the belts using lists. Here are the steps:
+
+# Define the function to accept two parameters for our two lists of numbers
+# Check if the length of the first list is greater than or equal to the length of the second list
+# If true, then return the last element from the first list. Otherwise, return the last element from the second list
+# Write a function named larger_list that has two parameters named lst1 and lst2.
+
+# The function should return the last element of the list that contains more elements. If both lists are the same size, then return the last element of lst1.
+
+# Use the below code snippet to start the problem:
+
+# #Write your function here
+
+
+# #Uncomment the line below when your function is done
+# #print(larger_list([4, 10, 2, 5], [-10, 2, 5, 10]))
