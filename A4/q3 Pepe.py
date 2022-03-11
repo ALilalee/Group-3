991b4b0203ebd8eac711cfde421bb5be14f8fb0d
Pelagie4 <tpg23615@email.vccs.edu>
2022-03-10 20:53:01 -0500

    q3 done

--- A4/q3 Pepe.py
+++ A4/q3 Pepe.py
@@ -1,4 +1,3 @@
-hello
 # 3. Larger List
 # Let’s say we are working with two conveyor belts that contain items represented by a numerical ID. If one conveyor belt contains more items than the other, then we need to return the ID of the last item on that belt. In the case where they have the same number of items, return the last item from the first conveyor belt. In our code, we can represent the belts using lists. Here are the steps:
 
@@ -16,3 +15,11 @@ hello
 
 # #Uncomment the line below when your function is done
 # #print(larger_list([4, 10, 2, 5], [-10, 2, 5, 10]))
+
+def larger_list(lst1, lst2) :
+    if len(lst1) >= len(lst2) :
+        return lst1[-1]
+    else:
+        return lst2[-1]
+print(larger_list([4, 10, 2, 5], [-10, 2, 5, 10]))
+
