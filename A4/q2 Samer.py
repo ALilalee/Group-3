4b4e35f891c7671e805d51c23f40ac5a295ba0e4
ALilalee <ali.lalee@yahoo.com>
2022-03-14 17:38:29 -0400

    q2 finished - Ali

--- A4/q2 Samer.py
+++ A4/q2 Samer.py
@@ -1,5 +1,8 @@
 # 2. Append Sum
-# Let’s create a function that calculates the sum of the last two elements of a list and appends it to the end. After doing so, it will repeat this process two more times and return the resulting list. You can choose to use a loop or manually use three lines. Here are the steps we need:
+# Let’s create a function that calculates the sum of the last two elements of a 
+# list and appends it to the end. After doing so, it will repeat this process 
+# two more times and return the resulting list. You can choose to use a loop or 
+# manually use three lines. Here are the steps we need:
 
 # Define the function to accept one parameter for our list of numbers
 # Add the last and second to last elements from our list together
@@ -15,7 +18,12 @@
 # Use the below code snippet to start the problem:
 
 # #Write your function here
-
+def append_sum(lst):
+    count = 0
+    while count < 3:
+        count += 1
+        lst.append(lst[-1] + lst[-2])
+    return lst
 
 # #Uncomment the line below when your function is done
-# #print(append_sum([1, 1, 2]))
+print(append_sum([1, 1, 2]))
