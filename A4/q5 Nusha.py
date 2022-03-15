64479ec5bd8bd4d640c990bf3fe0efa27cc9e81d
ALilalee <ali.lalee@yahoo.com>
2022-03-14 21:11:56 -0400

    clean

--- A4/q5 Nusha.py
+++ A4/q5 Nusha.py
@@ -9,17 +9,16 @@
 # Return the sorted and combined list
 # Write a function named combine_sort that has two parameters named lst1 and lst2.
 
-# The function should combine these two lists into one new list and sort the result. Return the new sorted list.
+# The function should combine these two lists into one new list and sort the result.
+#  Return the new sorted list.
 
 # Use the below code snippet to start the problem:
 
 #Write your function here
-
-
-#Uncomment the line below when your function is done
-#print(combine_sort([4, 10, 2, 5], [-10, 2, 5, 10]))
-
 new_list=[]
 def combine_sort(lst1, lst2):
     new_list=sorted(lst1+lst2)
-print (new_list)
+
+#Uncomment the line below when your function is done
+print(combine_sort([4, 10, 2, 5], [-10, 2, 5, 10]))
+
